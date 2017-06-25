5e8f5ebca2583eb069a322652f0547d319fad96a
Artem.K <kuznecov_15@mail.ru>
2017-06-25 23:56:35 +0300

    The first programm "Hello world" was created

---
+++ src/main/java/task_01/FirstProgram.java
@@ -0,0 +1,13 @@
+package task_01;
+
+/**
+ * Created by artem on 25.06.17.
+ */
+public class FirstProgram {
+
+    public static void main(String[] args) {
+
+        System.out.print("Hello world");
+
+    }
+}
